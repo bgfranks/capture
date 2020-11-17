7d4ed1086f177a64932eab33ebd5d7b44a98b84e
Brandon Franks <Brandon@Brandons-MacBook-Pro.local>
2020-11-16 19:45:04 -0500

    created animations for the home page on load

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,31 +3,37 @@ import { motion } from "framer-motion"
 import { About, Description, Hide, Image } from "../styles"
 import home1 from "../img/home1.png"
 
+import { titleAnimation, fade, photoAnimation } from "../animation"
+
 const AboutSection = () => {
   return (
     <About>
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="Guy with a camera" />
+        <motion.img
+          variants={photoAnimation}
+          src={home1}
+          alt="Guy with a camera"
+        />
       </Image>
     </About>
   )
